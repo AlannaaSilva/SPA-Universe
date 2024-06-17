4b47feda9440d2df8aa670c2ba7b2da2003982f3
Alanna Silva <alannacostas55@gmail.com>
2024-06-17 18:42:06 -0300

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const router = new Router()
 router.add('/',"/pages/home.html")
 router.add('/universe',"/pages/universe.html")
 router.add('/explore',"/pages/explore.html")
-router.add(404, "/pages/404.html");
+
 
 
 
